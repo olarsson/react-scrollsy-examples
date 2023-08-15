cffdf6f161979d0dbfde3cf8baca70256f2d5d79
folofolo <olarsson@live.com>
2023-08-15 20:39:58 +0200

    relative path fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
   },
   plugins: [react()],
 })
